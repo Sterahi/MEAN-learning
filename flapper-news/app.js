c4af683446b695bca76f7a3a67e58206ced725e4
Elizabeth <Elizabeth.Murray@class1inc.com>
2016-09-12 10:40:36 -0400

    Fixed issues with Models not being registered Added Upvoting functionality Begun work on comment upvote functionality.

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -4,16 +4,20 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+
 var mongoose = require('mongoose')
+//Connect to local DB
+mongoose.connect('mongodb://localhost/news')
+//Model declaration has to come before routes declaration.
+require('./models/Comments')
+require('./models/Posts')
 
 var routes = require('./routes/index')
 var users = require('./routes/users')
 
 var app = express()
 
-require('./models/Post')
-require('./models/Comments')
-mongoose.connect('mongodb://localhost/news')
+// Require Mongoos and Models
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,29 +1,94 @@
 var express = require('express')
 var router = express.Router()
 var mongoose = require('mongoose')
-var post = mongoose.model('Post')
-var comment = mongoose.model('Comment')
+var Post = mongoose.model('Post')
+var Comment = mongoose.model('Comment')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
-})
+//Default Route. (keeping for references sake)
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-router.get('/posts', function (req, res, next) {
-  post.find(function (err, posts) {
-    if (err) { return next(err) }
-
+// Route for loading posts array (in JSON)
+router.get('/posts', function(req, res, next){
+  Post.find(function(err, posts){
+    if(err) { return next(err) }
     res.json(posts)
   })
 })
 
-router.post('/posts', function (req, res, next) {
+// Route for adding Posts to DB!
+router.post('/posts', function(req, res, next){
   var post = new Post(req.body)
-
-  post.save(function (err, post) {
-    if (err) { return next(err) }
-
+  // Error checking on posts.
+  post.save(function(err, post){
+    if(err){ return next(err) }
+    // Returning the post as a JSON
     res.json(post)
   })
 })
-module.exports = router
+
+//Calls the findById method on any post that has the /:post directive.
+router.param('post', function(req, res, next, id){
+  var query = Post.findById(id)
+  query.exec (function (err, post){
+    if (err) { return next (err) }
+    if (!post) { return next( new Error ('cant\'t find post'))}
+    req.post = post;
+    return next()
+  })
+})
+
+//Route to retrieve specific posts
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post)
+})
+
+// Calls the Upvote Method from /models/Posts.js
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err) }
+    res.json(post)
+  })
+})
+
+router.post('/posts/:post/comments', function (req, res,next) {
+  var comment = new Comment(req.body)
+  comment.post = req.post
+  comment.save (function (err, comment){
+    if(err){ return next(err) }
+    req.post.comments.push(comment)
+    req.post.save(function (err, post) {
+      if (err) { return next(err) }
+      res.json(comment)
+    })
+  })
+})
+
+// Calls the Upvote Method from /models/Posts.js
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err) }
+    res.json(post)
+  })
+})
+
+//Calls the findById method on any post that has the /:comment directive.
+router.param('comment', function(req, res, next, id){
+  var query = Comment.findById(id)
+  query.exec (function (err, post){
+    if (err) { return next (err) }
+    if (!post) { return next( new Error ('cant\'t find post'))}
+    req.comment = comment;
+    return next()
+  })
+})
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+  req.comment.upvote(function(err, comment){
+    if (err) {return next(err)}
+  })
+})
+
+module.exports = router;
