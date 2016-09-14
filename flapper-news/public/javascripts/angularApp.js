e85750cadc80a62966b69c5bb37cabfe4f16eaf5
Elizabeth <Elizabeth.Murray@class1inc.com>
2016-09-14 15:48:56 -0400

    Comment upvoting enabled

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -29,74 +29,71 @@ app.config([
   }])
 
 app.factory('posts', ['$http', function ($http) {
+  /* Initial Declaration of Posts Array */
   var o = {
     posts: []
   }
 
+  /* Get All posts for the homepage */
   o.getAll = function () {
     return $http.get('/posts').success(function (data) {
       angular.copy(data, o.posts)
     })
   }
 
+  /* Create new link Post */
   o.create = function (post) {
     return $http.post('/posts', post).success(function (data) {
       o.posts.push(data)
     })
   }
+
+  /* Get single post */
   o.get = function (id) {
     return $http.get('/posts/' + id).then(function (res) {
       return res.data
     })
   }
-app.config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: '/home.html',
-        controller: 'MainCtrl',
-        resolve: {
-          postPromise: ['posts', function (posts) {
-            return posts.getAll()
-          }]
-        }
-      })
-      .state('posts', {
-        url: '/posts/{id}',
-        templateUrl: '/posts.html',
-        controller: 'PostsCtrl',
-        resolve: {
-          post: ['$stateParams', 'posts', function ($stateParams, posts) {
-            return posts.get($stateParams._id)
-          }]
-        }
-      })
-    $urlRouterProvider.otherwise('home')
-  }])
 
-  o.upvote = function (post) {
-    return $http.put('/posts/' + post._id + '/upvote')
-      .success(function (data) {
-        post.upvotes += 1
-      })
-  }
-
+  /* Add comment to front page (starts with 0 upvotes) */
   o.addComment = function (id, comment) {
     return $http.post('/posts/' + id + '/comments', comment)
   }
 
+  /* Upvoting  post*/
+  o.upvote = function (post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+    .success(function (data) {
+      post.upvotes += 1
+    })
+  }
+
+  /* Upvote Comment. Currently broken and troubleshooting.
+     All variables are being passed to function properly, still not upvoting.*/
+  o.upvoteComment =
+  function (post, comment) {
+    console.log('Reached UpvoteComment')
+    console.log(post._id)
+    console.log(comment._id)
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+      .success(function (data) {
+        comment.upvotes += 1
+      })
+  }
+
+  /* Returning o to return all of Posts methods.*/
   return o
 }])
+
+/* Controller for homepage. */
 app.controller('MainCtrl', [
   '$scope',
-  '$stateParams',
   'posts',
-  function ($scope, $stateParams, posts) {
+  function ($scope, posts) {
+    /* Binding Scope.posts to posts.posts*/
     $scope.posts = posts.posts
 
+    /* Function to add post to using posts(o).create */
     $scope.addPost = function () {
       if (!$scope.title || $scope.title === '') { return }
       posts.create({
@@ -106,6 +103,7 @@ app.controller('MainCtrl', [
       $scope.title = ''
       $scope.link = ''
     }
+    /* Calling posts(o).upvote to write to Db */
     $scope.incrementUpvotes = function (post) {
       posts.upvote(post)
     }
@@ -116,10 +114,13 @@ app.controller('PostsCtrl', [
   'posts',
   'post',
   function ($scope, posts, post) {
+    /* Binding $scope.post to o.post */
     $scope.post = post
+
     $scope.incrementUpvotes = function (comment) {
       posts.upvoteComment(post, comment)
     }
+
     $scope.addComment = function () {
       if ($scope.body === '') { return }
       posts.addComment(post._id, {
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -5,73 +5,73 @@ var Post = mongoose.model('Post')
 var Comment = mongoose.model('Comment')
 
 /* GET home page. */
-//Default Route. (keeping for references sake)
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Default Route. (keeping for references sake)
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-//Calls the findById method on any post that has the /:post directive.
-router.param('post', function(req, res, next, id){
+// Calls the findById method on any post that has the /:post directive.
+router.param('post', function (req, res, next, id) {
   var query = Post.findById(id)
-  query.exec (function (err, post){
+  query.exec(function (err, post) {
     if (err) { return next(err) }
-    if (!post) { return next( new Error ('cant\'t find post'))}
-    req.post = post;
+    if (!post) { return next(new Error('cant\'t find post')) }
+    req.post = post
     return next()
   })
 })
 
-//Calls the findById method on any post that has the /:comment directive.
-router.param('comment', function(req, res, next, id){
+// Calls the findById method on any comment that has the /:comment directive.
+router.param('comment', function (req, res, next, id) {
   var query = Comment.findById(id)
-  query.exec (function (err, post){
-    if (err) { return next (err) }
-    if (!post) { return next( new Error ('cant\'t find post'))}
-    req.comment = comment;
+  query.exec(function (err, comment) {
+    if (err) { return next(err) }
+    if (!comment) { return next(new Error('cant\'t find comment')) }
+    req.comment = comment
     return next()
   })
 })
 
 // Route for loading posts array (in JSON)
-router.get('/posts', function(req, res, next){
-  Post.find(function(err, posts){
-    if(err) { return next(err) }
+router.get('/posts', function (req, res, next) {
+  Post.find(function (err, posts) {
+    if (err) { return next(err) }
     res.json(posts)
   })
 })
 
 // Route for adding Posts to DB!
-router.post('/posts', function(req, res, next){
+router.post('/posts', function (req, res, next) {
   var post = new Post(req.body)
   // Error checking on posts.
-  post.save(function(err, post){
-    if(err){ return next(err) }
+  post.save(function (err, post) {
+    if (err) { return next(err) }
     // Returning the post as a JSON
     res.json(post)
   })
 })
 
-//Route to retrieve specific posts
-router.get('/posts/:post', function(req, res) {
-  req.post.populate('comments', function(err, post){
+// Route to retrieve specific posts
+router.get('/posts/:post', function (req, res) {
+  req.post.populate('comments', function (err, post) {
     if (err) { return next(err) }
     res.json(req.post)
   })
 })
 
 // Calls the Upvote Method from /models/Posts.js
-router.put('/posts/:post/upvote', function(req, res, next) {
-  req.post.upvote(function(err, post){
+router.put('/posts/:post/upvote', function (req, res, next) {
+  req.post.upvote(function (err, post) {
     if (err) { return next(err) }
     res.json(post)
   })
 })
 
-router.post('/posts/:post/comments', function (req, res,next) {
+router.post('/posts/:post/comments', function (req, res, next) {
   var comment = new Comment(req.body)
   comment.post = req.post
-  comment.save (function (err, comment){
-    if(err){ return next(err) }
+  comment.save(function (err, comment) {
+    if (err) { return next(err) }
     req.post.comments.push(comment)
     req.post.save(function (err, post) {
       if (err) { return next(err) }
@@ -80,10 +80,11 @@ router.post('/posts/:post/comments', function (req, res,next) {
   })
 })
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
-  req.comment.upvote(function(err, comment){
-    if (err) {return next(err)}
+router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+  req.comment.upvote(function (err, comment) {
+    if (err) { return next(err) }
+    res.json(comment)
   })
 })
 
-module.exports = router;
+module.exports = router
